49348c0ec51f78f9ce82e21639c2ae7810839000
Sergey <sergeyshaykhullin@gmail.com>
2019-02-25 22:10:00 +0300

    1.0.0 Add basic healthchecks

---
+++ src/OtakuShelter.Health.Web/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace OtakuShelter.Health
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateWebHostBuilder(args).Build().Run();
+		}
+
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.ConfigureAppConfiguration(builder => 
+					builder.AddYamlFile("appsettings.yml"))
+				.UseStartup<Startup>();
+	}
+}
---
+++ src/OtakuShelter.Health.Web/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+using HealthChecks.UI.Client;
+
+namespace OtakuShelter.Health
+{
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddHealthChecks()
+				.AddUrlGroup(new Uri("http://account.otaku-shelter.ru/swagger"), "Account")
+				.AddUrlGroup(new Uri("http://profile.otaku-shelter.ru/swagger"), "Profile")
+				.AddUrlGroup(new Uri("http://manga.otaku-shelter.ru/swagger"), "Manga");
+				// .AddNpgSql("");
+
+			services.AddHealthChecksUI();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			app.UseHealthChecks("/health", new HealthCheckOptions
+			{
+				ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+			});
+			
+			app.UseHealthChecksUI();
+		}
+	}
+}
